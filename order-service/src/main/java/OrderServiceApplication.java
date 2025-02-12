b5eced0c5d303e332e06411be55fb28643b977db
Crusher95574 <97830371+Crusher95574@users.noreply.github.com>
2025-02-12 10:57:53 +0530

    Order service update v2

--- order-service/src/main/java/OrderServiceApplication.java
+++ order-service/src/main/java/OrderServiceApplication.java
@@ -1,6 +1,12 @@
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class OrderService {
-    
+@EnableJpaRepositories
+
+public class OrderServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(OrderServiceApplication.class);
+    }
 }
--- order-service/src/main/java/com/example/ecommerce_platform/order_service/model/Order.java
+++ order-service/src/main/java/com/example/ecommerce_platform/order_service/model/Order.java
@@ -1,2 +1,48 @@
-package com.example.ecommerce_platform.order_service.model;public class Order {
+package com.example.ecommerce_platform.order_service.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @NotBlank(message = "Order status is required")
+    private String status; // For example: PENDING, COMPLETED, CANCELLED
+
+    @NotNull(message = "Order date is required")
+    private LocalDateTime orderDate;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = true, message = "Price must be non-negative")
+    private Double totalAmount;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    // Convenience method to add an order item
+    public void addOrderItem(OrderItem item) {
+        item.setOrder(this);
+        this.orderItems.add(item);
+    }
 }
